8d5c158fd0772d96bf4cebddf3fc82f2472832ff
BesAnh <anhle3532@gmail.com>
2025-02-20 13:37:27 +0700

    update - util and excel export

---
+++ common/util/util.go
@@ -0,0 +1,180 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const MAX_LIMIT = 50_000
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+
+func ParseLimit(limit any) int {
+	var val = 10
+	var err error
+	if reflect.TypeOf(limit).Kind() == reflect.String {
+		str := ParseString(limit)
+		val, err = strconv.Atoi(str)
+		if err != nil {
+			val = 10
+		}
+	} else {
+		val = ParseInt(fmt.Sprintf("%d", limit))
+	}
+	if val < 0 {
+		val, _ = strconv.Atoi(fmt.Sprintf("%s", limit))
+	} else if val > MAX_LIMIT {
+		val = MAX_LIMIT
+	}
+	return val
+}
+
+func ParseOffset(offset any) int {
+	var val = 0
+	var err error
+	if reflect.TypeOf(offset).Kind() == reflect.String {
+		str := ParseString(offset)
+		val, err = strconv.Atoi(str)
+		if err != nil {
+			val = 0
+		}
+	} else {
+		val = ParseInt(fmt.Sprintf("%d", offset))
+	}
+	if val < 0 {
+		val = 0
+	}
+	return val
+}
+
+func ParseInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return i
+}
+
+func ParseAnyToString(value any) (string, error) {
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func ParseStringToAny(value string, dest any) error {
+	if err := json.Unmarshal([]byte(value), dest); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ParseAnyToAny(value any, dest any) error {
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(bytes, dest); err != nil {
+		return err
+	}
+	return nil
+}
+
+func ParseString(value any) string {
+	str, ok := value.(string)
+	if !ok {
+		return str
+	}
+	str = strings.Replace(str, "\n", "", -1)
+	str = strings.Trim(str, "\r\n")
+	str = strings.TrimSpace(str)
+	return str
+}
+
+func InArray(item any, array any) bool {
+	arr := reflect.ValueOf(array)
+	if arr.Kind() != reflect.Slice {
+		return false
+	}
+	for i := 0; i < arr.Len(); i++ {
+		if arr.Index(i).Interface() == item {
+			return true
+		}
+	}
+	return false
+}
+
+func GenerateRandomString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
+func ParseToAnyArray(value []string) []any {
+	result := make([]any, 0)
+	for _, v := range value {
+		result = append(result, v)
+	}
+	return result
+}
+
+func TimeToString(valueTime time.Time) string {
+	return TimeToStringLayout(valueTime, "2006-01-02 15:04:05")
+}
+
+func TimeToStringLayout(valueTime time.Time, layout string) string {
+	return valueTime.Format(layout)
+}
+
+func InArrayContains(item string, array []string) bool {
+	for _, v := range array {
+		if strings.Contains(item, v) {
+			return true
+		}
+	}
+	return false
+}
+
+func ParseStructToByte(data any) ([]byte, error) {
+	value, err := ParseAnyToString(data)
+	if err != nil {
+		return nil, err
+	}
+	byteSlice := []byte(value)
+	return byteSlice, nil
+}
+
+func ParseQueryArray(slice []string) []string {
+	result := make([]string, 0)
+	for _, v := range slice {
+		if len(v) > 0 {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func ContainKeywords(content string, keywords []string) bool {
+	for _, keyword := range keywords {
+		if strings.Contains(content, keyword) {
+			return true
+		}
+	}
+	return false
+}
+
+func SliceToMap[T comparable](slice []T) map[T]bool {
+	set := make(map[T]bool)
+	for _, v := range slice {
+		set[v] = true
+	}
+	return set
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.2
 require github.com/gookit/slog v0.5.7
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -15,6 +16,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -23,28 +25,41 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.4 // indirect
+	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
 	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	golang.org/x/arch v0.8.0 // indirect
-	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/mod v0.23.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	mellium.im/sasl v0.3.2 // indirect
 )
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/go-sql-driver/mysql v1.9.0
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/gookit/goutil v0.6.17 // indirect
 	github.com/gookit/gsr v0.1.0 // indirect
+	github.com/uptrace/bun v1.2.10
+	github.com/uptrace/bun/dialect/mysqldialect v1.2.10
+	github.com/uptrace/bun/dialect/pgdialect v1.2.10
+	github.com/uptrace/bun/driver/pgdriver v1.2.10
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/xuri/excelize/v2 v2.9.0
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
