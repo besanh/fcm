29c7b7eadb1af96f70dd06ff991f0fbb3a9b6f3d
BesAnh <anhle3532@gmail.com>
2025-02-21 21:40:55 +0700

    update - init mongo in main.go

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-func init() {
+import (
+	"fcm/common/env"
+	"fcm/common/log"
+	"fcm/internals/mongodb"
+)
 
+func init() {
+	initMongoDb()
+	initFcm()
+}
+
+func initMongoDb() {
+	mongodbConfig := mongodb.MongoDBConfig{
+		Username: env.GetStringENV("FCM_MONGODB_USERNAME", "root"),
+		Password: env.GetStringENV("FCM_MONGODB_PASSWORD", "anhle@!*2025"),
+		Host:     env.GetStringENV("FCM_MONGODB_HOST", "localhost"),
+		Port:     env.GetIntENV("FCM_MONGODB_PORT", 27017),
+		Database: env.GetStringENV("FCM_MONGODB_DATABASE", "fcm"),
+	}
+
+	var err error
+	// var db mongodb.IMongoDBClient
+	_, err = mongodb.NewMongoDBClient(mongodbConfig)
+	if err != nil {
+		log.Errorf("mongodb connect error: %v", err)
+		panic(err)
+	}
+}
+
+func initFcm() {
+	// fcm
 }
 
 func main() {
